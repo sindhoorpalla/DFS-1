e832531509aa536cbb3370ac9959232bf3b5576e
sindhoorpalla <53406467+sindhoorpalla@users.noreply.github.com>
2025-07-19 17:22:01 -0700

    PR

--- Sample.java
+++ Sample.java
@@ -1,7 +1,40 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity :O(M*N)
+// Space Complexity : O(M*N)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+
+
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int rows = image.length;
+        int cols = image[0].length;
+        int startColor = image[sr][sc];
+
+        // If the new color is same as the starting color, no need to do anything
+        if (startColor == color) return image;
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{sr, sc});
+        image[sr][sc] = color;
+
+        // Directions: up, down, left, right
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int r = curr[0], c = curr[1];
+
+            for (int[] dir : directions) {
+                int nr = r + dir[0], nc = c + dir[1];
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && image[nr][nc] == startColor) {
+                    queue.offer(new int[]{nr, nc});
+                    image[nr][nc] = color;
+                }
+            }
+        }
+
+        return image;
+    }}
